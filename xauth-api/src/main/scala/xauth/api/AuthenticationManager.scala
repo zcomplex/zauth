95fae1f1e9a0b9f4c7f29de77fa2c2ad78f05710
enrico <enrico.russo.84@gmail.com>
2025-11-14 17:58:04 +0200

    auth aspects for workspaces, clients, users and roles

--- xauth-api/src/main/scala/xauth/api/AuthenticationManager.scala
+++ xauth-api/src/main/scala/xauth/api/AuthenticationManager.scala
@@ -1,6 +1,33 @@
+/*
+ * Copyright (C) 2025-Present ZAuth.
+ * This file is part of ZAuth, Multi-Tenant Authentication System.
+ *
+ * This software is released under the ZAuth License V1, which is based on the
+ * GNU General Public License version 3 (GPLv3) as published by the Free Software
+ * Foundation, with an additional "No SaaS" clause.
+ *
+ * You may redistribute and/or modify it under the terms of the GPLv3 as
+ * published by the Free Software Foundation, with the added restriction that
+ * this software may not be provided as a public network service (SaaS,
+ * DBaaS, API, or similar) without prior written authorization from the author.
+ *
+ * THERE IS NO WARRANTY FOR THE PROGRAM, TO THE EXTENT PERMITTED BY
+ * APPLICABLE LAW. EXCEPT WHEN OTHERWISE STATED IN WRITING THE COPYRIGHT
+ * HOLDERS AND/OR OTHER PARTIES PROVIDE THE PROGRAM "AS IS" WITHOUT WARRANTY
+ * OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE. THE ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE PROGRAM
+ * IS WITH YOU. SHOULD THE PROGRAM PROVE DEFECTIVE, YOU ASSUME THE COST OF
+ * ALL NECESSARY SERVICING, REPAIR OR CORRECTION.
+ *
+ * This software is released under ZAuth License V1.
+ * See LICENSE for full terms.
+ */
 package xauth.api
 
+import xauth.api.ClientResolver.CtxOut
 import xauth.api.Main.validateEnv
+import xauth.api.UserResolver.{CxtOut, Env}
 import xauth.api.jwt.JwtHelper
 import xauth.core.application.usecase.WorkspaceRegistry
 import xauth.core.common.model.AuthRole
@@ -14,23 +41,74 @@ import zio.http.*
 
 final case class ClientCredentials(id: String, secret: String)
 
-class WorkspaceContext(val workspace: Workspace)
-class ClientContext(override val workspace: Workspace, val client: Client) extends WorkspaceContext(workspace)
-class UserContext(override val workspace: Workspace, val user: User) extends WorkspaceContext(workspace)
+/** Workspace information for recognized workspace requests. */
+sealed class WorkspaceContext(val workspace: Workspace)
 
-class AuthenticationManager:
+/** Client information for recognized client (http-basic) requests. */
+sealed class ClientContext(override val workspace: Workspace, val client: Client) extends WorkspaceContext(workspace)
 
-  val WorkspaceAspect: HandlerAspect[WorkspaceRegistry, WorkspaceContext] =
+/** User information for recognized user requests. */
+sealed class UserContext(override val workspace: Workspace, val user: User) extends WorkspaceContext(workspace)
+
+/** Role information for recognized user roles requests. */
+sealed class RoleContext(override val workspace: Workspace, override val user: User, val roles: Seq[AuthRole])
+  extends UserContext(workspace, user)
+
+object AuthenticationManager:
+
+  type WorkspaceAspectEnv = WorkspaceResolver.Env
+  type WorkspaceAspectCtxOut = WorkspaceResolver.CtxOut
+
+  /**
+   * Workspace Request Flow
+   * Performs security checks for ingoing workspace requests.
+   */
+  val WorkspaceAspect: HandlerAspect[WorkspaceAspectEnv, WorkspaceAspectCtxOut] =
     WorkspaceResolver.aspect
 
-  val ClientAspect: HandlerAspect[WorkspaceRegistry & ClientService, (WorkspaceContext, ClientContext)] =
-//    WorkspaceAspect ++ ClientResolver.aspect
-    ???
+  type ClientAspectEnv = WorkspaceAspectEnv & ClientResolver.Env
+  type ClientAspectCtxOut = (WorkspaceAspectCtxOut, ClientResolver.CtxOut)
 
-  val UserAspect: HandlerAspect[WorkspaceRegistry & JwtHelper & UserService, (WorkspaceContext, UserContext)] =
-//    WorkspaceAspect ++ UserResolver.aspect
-    ???
-  
-  def RoleAspect(roles: AuthRole*): HandlerAspect[WorkspaceRegistry & JwtHelper & UserService, (WorkspaceContext, UserContext, UserContext)] =
-//    UserAspect ++ RoleChecker.roleAspect(roles*)
-    ???
+  /**
+   * Client Request Flow
+   * Performs security checks for ingoing client requests,
+   * checks if http-basic authentication satisfies client requirements
+   * like credentials and status.
+   *
+   * This aspect performs checks for:
+   *   - Workspace 
+   */
+  val ClientAspect: HandlerAspect[ClientAspectEnv, ClientAspectCtxOut] =
+    WorkspaceAspect ++ ClientResolver.aspect
+
+  type UserAspectEnv = WorkspaceAspectEnv & UserResolver.Env
+  type UserAspectCtxOut = (WorkspaceAspectCtxOut, UserResolver.CxtOut)
+
+  /**
+   * User Request Flow
+   * Performs security checks for ingoing user requests,
+   * checks if the request user is recognized, active
+   * and if his credentials are valid.
+   * 
+   * This aspect performs checks for:
+   *   - Workspace
+   *   - User
+   */
+  val UserAspect: HandlerAspect[UserAspectEnv, UserAspectCtxOut] = 
+    WorkspaceAspect ++ UserResolver.aspect
+
+  type RoleAspectEnv = UserAspectEnv & RoleChecker.Env
+  type RoleAspectCtxOut = (WorkspaceAspectCtxOut, UserResolver.CxtOut, RoleChecker.CtxOut)
+
+  /**
+   * Role Request Flow
+   * Performs security checks for ingoing user roles requests,
+   * checks if the recognized user matches at least one of the specified roles.
+   *
+   * This aspect performs checks for:
+   *   - Workspace
+   *   - User
+   *   - Roles
+   */
+  def RoleAspect(roles: AuthRole*): HandlerAspect[RoleAspectEnv, RoleAspectCtxOut] =
+    UserAspect ++ RoleChecker.roleAspect(roles*)
