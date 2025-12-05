57467117f1ce7b772b2d1c366673e0dd9499550c
enrico <enrico.russo.84@gmail.com>
2025-12-05 14:11:57 +0200

    core: minor improvements for workspace registry and comments

--- xauth-core/src/main/scala/xauth/core/application/usecase/WorkspaceRegistry.scala
+++ xauth-core/src/main/scala/xauth/core/application/usecase/WorkspaceRegistry.scala
@@ -1,3 +1,28 @@
+/*
+ * Copyright (C) 2025-Present ZAuth.
+ * This file is part of ZAuth, Multi-Tenant Authentication System.
+ *
+ * This software is released under the ZAuth License V1, which is based on the
+ * GNU General Public License version 3 (GPLv3) as published by the Free Software
+ * Foundation, with an additional "No SaaS" clause.
+ *
+ * You may redistribute and/or modify it under the terms of the GPLv3 as
+ * published by the Free Software Foundation, with the added restriction that
+ * this software may not be provided as a public network service (SaaS,
+ * DBaaS, API, or similar) without prior written authorization from the author.
+ *
+ * THERE IS NO WARRANTY FOR THE PROGRAM, TO THE EXTENT PERMITTED BY
+ * APPLICABLE LAW. EXCEPT WHEN OTHERWISE STATED IN WRITING THE COPYRIGHT
+ * HOLDERS AND/OR OTHER PARTIES PROVIDE THE PROGRAM "AS IS" WITHOUT WARRANTY
+ * OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE. THE ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE PROGRAM
+ * IS WITH YOU. SHOULD THE PROGRAM PROVE DEFECTIVE, YOU ASSUME THE COST OF
+ * ALL NECESSARY SERVICING, REPAIR OR CORRECTION.
+ *
+ * This software is released under ZAuth License V1.
+ * See LICENSE for full terms.
+ */
 package xauth.core.application.usecase
 
 import xauth.core.application.usecase.WorkspaceRegistry.WContext
@@ -7,7 +32,7 @@ import xauth.core.spi.MessagingProvider
 import xauth.core.spi.MessagingProvider.ProviderRegistry
 import xauth.util.{Registry, Uuid}
 import zio.stm.TMap
-import zio.{UIO, ZIO, ZLayer}
+import zio.{RLayer, UIO, ZIO, ZLayer}
 
 import scala.reflect.ClassTag
 
@@ -44,7 +69,7 @@ object WorkspaceRegistry:
 
   private type WEntry = (id: Uuid, context: WContext)
 
-  val layer: ZLayer[ProviderRegistry & WorkspaceRepository, Throwable, WorkspaceRegistry] =
+  val layer: RLayer[ProviderRegistry & WorkspaceRepository, WorkspaceRegistry] =
     ZLayer.fromZIO:
       for
         registry   <- ZIO.service[ProviderRegistry]
@@ -61,7 +86,8 @@ object WorkspaceRegistry:
             _ <- ZIO logInfo s"loading workspace '${w.slug}' with ${entry.context.providers.size} providers"
           yield entry
 
-        data <- TMap // todo: use zio cache
+        // todo: use zio cache and refresh workspaces (or use a queue for workspaces that need to be refreshed)
+        data <- TMap
           .fromIterable[Uuid, WContext](entries.map(e => (e.id, e.context)))
           .commit
 
