17fcc60d59f015212e8eaae7f7a116af19f1867d
enrico <enrico.russo.84@gmail.com>
2025-12-16 09:11:07 +0200

    enum val enum docs and license

--- xauth-util/src/main/scala/xauth/util/EnumFromVal.scala
+++ xauth-util/src/main/scala/xauth/util/EnumFromVal.scala
@@ -1,5 +1,47 @@
+/*
+ * Copyright (C) 2025-Present ZAuth.
+ * This file is part of ZAuth, Multi-Tenant Authentication System.
+ *
+ * This software is released under the ZAuth License V1, which is based on the
+ * GNU General Public License version 3 (GPLv3) as published by the Free Software
+ * Foundation, with an additional "No SaaS" clause.
+ *
+ * You may redistribute and/or modify it under the terms of the GPLv3 as
+ * published by the Free Software Foundation, with the added restriction that
+ * this software may not be provided as a public network service (SaaS,
+ * DBaaS, API, or similar) without prior written authorization from the author.
+ *
+ * THERE IS NO WARRANTY FOR THE PROGRAM, TO THE EXTENT PERMITTED BY
+ * APPLICABLE LAW. EXCEPT WHEN OTHERWISE STATED IN WRITING THE COPYRIGHT
+ * HOLDERS AND/OR OTHER PARTIES PROVIDE THE PROGRAM "AS IS" WITHOUT WARRANTY
+ * OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE. THE ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE PROGRAM
+ * IS WITH YOU. SHOULD THE PROGRAM PROVE DEFECTIVE, YOU ASSUME THE COST OF
+ * ALL NECESSARY SERVICING, REPAIR OR CORRECTION.
+ *
+ * This software is released under ZAuth License V1.
+ * See LICENSE for full terms.
+ */
 package xauth.util
 
+/**
+ * Simple trait that defines methods to obtain enum instance
+ * values and also to obtain correspondent enum instance from a value.
+ * 
+ * Useful for reading values.
+ * 
+ * Enum definition using [[EnumVal]]:
+ * <pre>
+ * enum ContactType(val value: String) extends EnumVal[String]:
+ *   case Email extends ContactType("EMAIL")
+ *   ...
+ * </pre>
+ * usage:
+ * <pre>
+ * object ContactType extends EnumFromVal[ContactType, String]
+ * </pre>
+ */
 trait EnumFromVal[E <: EnumVal[A], A]:
   def values: Array[E]
 
